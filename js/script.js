f817b5989850fac7a17280549e056f13e07bfbe9
Phung Hoang <phunghoangvnuit@gmail.com>
2023-12-28 16:59:25 +0700

    v0.0.6 - Back to the Past

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+}
+
+let loginForm = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick = () =>{
+    loginForm.classList.remove('active');
+}
+
 window.onscroll = () => {
 
     searchForm.classList.remove('active');
@@ -18,9 +34,18 @@ window.onload = () => {
         document.querySelector('.header .header-2').classList.remove('active');
     }
 
+    fadeOut();
+
 }
 
-/*bookslider-handling*/
+function loader(){
+    document.querySelector('.loader-container').classList.add('active');
+}
+
+function fadeOut(){
+    setTimeout(loader, 4000);
+}
+
 var swiper = new Swiper(".books-slider", {
     loop: true,
     centeredSlides: true,
@@ -41,8 +66,7 @@ var swiper = new Swiper(".books-slider", {
     },
 });
 
-/*bestseller-slider-handling*/
-var swiper = new Swiper(".bestseller-slider", {
+var swiper = new Swiper(".featured-slider", {
     spaceBetween: 10,
     loop: true,
     centeredSlides: true,
@@ -66,6 +90,71 @@ var swiper = new Swiper(".bestseller-slider", {
         },
         1024: {
             slidesPerView: 4,
+        },
+    },
+});
+
+var swiper = new Swiper(".arrivals-slider", {
+    spaceBetween: 10,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+var swiper = new Swiper(".reviews-slider", {
+    spaceBetween: 10,
+    grabCursor: true,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+var swiper = new Swiper(".blogs-slider", {
+    spaceBetween: 10,
+    grabCursor: true,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
         },
     },
 });
