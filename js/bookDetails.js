2dc754f2cc66d4099b62effac0c5c86d6ef4ce2e
Phung Hoang <phunghoangvnuit@gmail.com>
2024-01-18 17:21:13 +0700

    v0.1.4

--- js/bookDetails.js
+++ js/bookDetails.js
@@ -39,3 +39,61 @@
 })
 })
 })
+
+/*Axios bookDetails*/
+function fetchBookDetails() {
+    const endpoint = `http://localhost:8080/api/books/2`;
+    // NOTE: const endpoint = `http://localhost:8080/api/books/{book_id}`;
+
+    // Send an HTTP GET request to the backend endpoint
+    axios.get(endpoint)
+        .then(response => {
+            // Handle the response data
+            const bookDetails = response.data;
+            displayBookDetails(bookDetails);
+        })
+        .catch(error => {
+            // Handle errors
+            console.error('Error fetching book details:', error);
+            alert('Error fetching book details. Please try again.');
+        });
+}
+
+function displayBookDetails(bookDetails) {
+    const bookContainer = document.querySelector('.bookDetails-body');
+
+    // Update your HTML or manipulate the DOM to display book details
+    bookContainer.innerHTML = `
+      <div class="bookDetails-left">
+        <!--<div class="bookDetails-imageDEMO" style="width: 300px; height: 300px; border-style: solid; border: none; background-color: red"></div>-->
+        <img src="image/produc-1.jpg">
+      </div>
+      <div class="bookDetails-right">
+        <p class="bookDetails-title">${bookDetails.title}</p>
+        <div class="bookDetails-info-container">
+          <p class="bookDetails-info">Author: <span>${bookDetails.author}</span></p>
+          <p class="bookDetails-info">Category: <span>${bookDetails.categoryId}</span></p>
+          <p class="bookDetails-info">In Stock: <span>${bookDetails.inStock}</span></p>
+        </div>
+        <div class="bookDetails-price">
+          <p class="bookDetails-newPrice">${bookDetails.price}đ</p>
+          <p class="bookDetails-oldPrice">${bookDetails.oldPrice}</p>
+          <p class="bookDetails-changePercent">${Math.round((bookDetails.price - bookDetails.oldPrice)/bookDetails.price)}</p>
+        </div>
+        <div class="bookDetails-AddToCart">
+          <div class="bookDetails-AddToCart-quantity">
+            <p>Quantity: </p>
+            <input class="bookDetails-AddToCart-quantityInput" type="text" placeholder="1">
+          </div>
+          <button class="bookDetails-AddToCart-add">
+            <p>Add to Cart</p>
+            <p href="#" class="fas fa-shopping-cart"></p>
+          </button>
+        </div>
+      </div>
+    `;
+    // You can add more details as needed
+}
+fetchBookDetails();
+displayBookDetails(1);
+//Note: displayBookDetails({book_id})
