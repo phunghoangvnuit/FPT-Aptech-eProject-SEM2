383fe367d9b8010329b354b7ce08ba9afd820052
Phung Hoang <phunghoangvnuit@gmail.com>
2024-01-21 20:11:20 +0700

    v0.2.1

--- js/bookCategories.js
+++ js/bookCategories.js
@@ -41,25 +41,38 @@ dropdown.forEach(dropdown => {
     })
 })
 
-/* 3> Handle Display CategoryName */
-function changeCategory(categorySelectionId,categorySelectionName) {
-    document.querySelector('.categoryNameDisplay').innerHTML = `${categorySelectionName}`;
+/* 3> Handle Display CategorySelection */
+function displayCategorySelection (categoryId) {
+    if(categoryId == 0){
+        document.querySelector('.categoryNameDisplay').innerHTML = 'All Categories';
+    } else if (categoryId == 1){
+        document.querySelector('.categoryNameDisplay').innerHTML = 'Business & Finance';
+    } else if (categoryId == 2){
+        document.querySelector('.categoryNameDisplay').innerHTML = 'Personal Development';
+    } else if (categoryId == 3){
+        document.querySelector('.categoryNameDisplay').innerHTML = 'Novel & Fiction';
+    } else if (categoryId == 4){
+        document.querySelector('.categoryNameDisplay').innerHTML = 'Biography';
+    } else if (categoryId == 5){
+        document.querySelector('.categoryNameDisplay').innerHTML = 'Education & Reference';
+    }
 
-    const allCategoryButton = document.querySelectorAll('.categorySelection');
-    allCategoryButton.forEach((categoryButton)=>{
-        categoryButton.classList.remove('activeCategoryButton')
+    const categoriesButton = document.querySelectorAll('.categorySelection');
+    categoriesButton.forEach((button) => {
+        button.classList.remove('activeCategoryButton');
     })
-
-    document.querySelector(`.Id_${categorySelectionId}`).classList.add('activeCategoryButton');
-
-    fetchBooksByCategory(categorySelectionId);
+    document.querySelector(`.Id_${categoryId}`).classList.add('activeCategoryButton');
 }
 
-/* 4> Add link to bookDetails */
+
+/* 4> Add Link To bookDetails */
 function redirectToAnotherPage(bookId) {
     window.location.href = `bookDetails.html?bookId=${bookId}`;
 }
 
+function redirectToGG() {
+    window.location.href = 'https://google.com.vn';
+}
 
 
 
@@ -70,9 +83,9 @@ function redirectToAnotherPage(bookId) {
 /*============ AXIOS ============*/
 /* 1>AXIOS - Display Books by Category */
 const urlParams = new URLSearchParams(window.location.search);
-const categoryId = urlParams.get('category');
+let categoryId = urlParams.get('category');
 const pageSize = 15; // Books per Page
-function fetchBooksByCategory(categoryId) {
+function fetchBooksByCategory() {
 
     if(categoryId == 0){
         const booksByCategoryEndpoint = `http://localhost:8080/api/books?size=${pageSize}`;
@@ -86,6 +99,8 @@ function fetchBooksByCategory(categoryId) {
                 console.error('Error fetching books by category:', error);
                 alert('Error fetching books by category. Please try again.');
             });
+        displayCategorySelection (categoryId);
+
     } else {
         const booksByCategoryEndpoint = `http://localhost:8080/api/books?category=${categoryId}&size=${pageSize}`;
 
@@ -98,6 +113,7 @@ function fetchBooksByCategory(categoryId) {
                 console.error('Error fetching books by category:', error);
                 alert('Error fetching books by category. Please try again.');
             });
+        displayCategorySelection (categoryId);
     }
 }
 function displayBookList(bookList) {
@@ -111,8 +127,8 @@ function displayBookList(bookList) {
     bookList.forEach(book => {
     const bookElement = document.createElement('div');
     bookElement.innerHTML = `
-    <div class="bookCategories-book" onclick="redirectToAnotherPage(${book.id})">
-          <div class="bookCategories-book-top">
+    <div class="bookCategories-book">
+          <div class="bookCategories-book-top" onclick="redirectToAnotherPage(${book.id})">
             <img src="../image/produc-1.jpg">
           </div>
 
@@ -126,7 +142,7 @@ function displayBookList(bookList) {
               <i class="fa-regular fa-star-half-stroke"></i>
             </div>
             <p class="bookCategories-bookPrice">${book.price} <span style="text-transform: none;">đ</span></p>
-            <a class="buttonToDetails">Add to Cart</a>
+            <button class="buttonToDetails" onclick="addToCart(${book.id},1,${book.inStock})">Add to Cart</button>
           </div>
         </div>
     `;
@@ -134,4 +150,23 @@ function displayBookList(bookList) {
 });
 }
 
-/*==> EXECUTE IN FIRST*/ fetchBooksByCategory(categoryId);
+/* 2>AXIOS - Add Book to Cart */
+const addToCart = async (bookId, bookQuantity, bookInStock) => {
+    if(bookQuantity > bookInStock){
+        alert('Out of Stock');
+        return;
+    }
+
+    try {
+        const response = await axios.put(`http://localhost:8080/api/carts/e1fb7652-9081-4a15-a4bc-991ac220682b`, {
+            bookId: bookId,
+            quantity: bookQuantity
+        });
+
+        console.log(response.data); // Assuming the server sends a success message
+    } catch (error) {
+        console.error('Error adding to cart:', error.message);
+    }
+};
+
+/*==> EXECUTE IN FIRST*/ fetchBooksByCategory();
